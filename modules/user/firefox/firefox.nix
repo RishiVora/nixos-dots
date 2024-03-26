526c9f6e1aebb36ca57813b7d94f77aad13def7f
Rishi <vorarishi22@gmail.com>
2024-03-27 01:23:58 +0530

    27 - firefox flag

--- modules/user/firefox/firefox.nix
+++ modules/user/firefox/firefox.nix
@@ -6,6 +6,7 @@
     programs.firefox = {
         enable = true;
         profiles.default.settings = {
+            "mousewheel.default.delta_multiplier_x" = 10;
             "mousewheel.default.delta_multiplier_y" = 20;
         };
     };
