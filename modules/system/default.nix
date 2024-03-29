3b45ee993e6356c95d2e0dccfdcfc96837fa7584
Rishi <vorarishi22@gmail.com>
2024-03-29 11:48:48 +0530

    32 - added bottles back

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -100,8 +100,6 @@
     # List packages installed in system profile. To search, run:
     # $ nix search wget
     environment.systemPackages = with pkgs; [
-        wineWowPackages.stable
-        #bottles
         unzip
         p7zip
     ];
