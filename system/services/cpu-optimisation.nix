42f6f97a35f026a68f516008b7dae900b5dc0fa3
Rishi <vorarishi22@gmail.com>
2024-09-03 11:21:36 +0530

    changed: TLP ON_BAT to balanced

--- system/services/cpu-optimisation.nix
+++ system/services/cpu-optimisation.nix
@@ -13,7 +13,7 @@
                 STOP_CHARGE_THRESH_BAT0=80;
 
                 PLATFORM_PROFILE_ON_AC = "performance";
-                PLATFORM_PROFILE_ON_BAT = "performance";
+                PLATFORM_PROFILE_ON_BAT = "balanced";
 
                 CPU_DRIVER_OPMODE_ON_AC = "passive";
                 CPU_DRIVER_OPMODE_ON_BAT = "passive";
@@ -21,9 +21,9 @@
                 CPU_BOOST_ON_AC = 1;
                 CPU_BOOST_ON_BAT = 0;
                 CPU_SCALING_GOVERNOR_ON_AC = "performance";
-                CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+                CPU_SCALING_GOVERNOR_ON_BAT = "balanced";
                 CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-                CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
+                CPU_ENERGY_PERF_POLICY_ON_BAT = "balanced";
             };
         };
     };
