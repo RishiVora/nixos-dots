576365686f07d186cda55802d49ba9c49b3d97a8
Rishi <vorarishi22@gmail.com>
2024-10-13 16:25:13 +0530

    modified: add window padding in kitty

--- home-manager/terminal/emulators/kitty/default.nix
+++ home-manager/terminal/emulators/kitty/default.nix
@@ -11,6 +11,10 @@
 
         # settings = import ./theme-oledppuccin-mocha.nix;
 
+        settings = {
+            window_padding_width = 8;
+        };
+
         extraConfig = ''
             map ctrl+backspace send_text all \x17
         '';
