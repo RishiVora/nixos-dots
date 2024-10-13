551244014ee95ef8f6c2be4cd4914f9e8779f063
Rishi <vorarishi22@gmail.com>
2024-10-14 02:30:08 +0530

    modified: deploy script

--- scripts/deploy.nix
+++ scripts/deploy.nix
@@ -9,9 +9,10 @@
             if test $status = 0
                 set gen $(nixos-rebuild list-generations --flake $FLAKE | grep -oP "[0-9]*(?= current)")
                 set timestamp $(date '+%x %X')
+                set branch $(git branch --show-current)
                 echo "$gen - $timestamp" >> $FLAKE/build.log
                 git add *
-                git commit -m "deployed $gen"
+                git commit -m "deployed $gen through $branch"
             else
                 echo "Failed to deploy!"
             end
