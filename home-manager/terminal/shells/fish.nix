21cd9040d6da732700ac869bb19553d5348b7409
Rishi <vorarishi22@gmail.com>
2024-10-15 18:41:51 +0530

    fish: QOL shell aliases

--- home-manager/terminal/shells/fish.nix
+++ home-manager/terminal/shells/fish.nix
@@ -4,5 +4,14 @@
 }: {
     programs.fish = {
         enable = true;
+
+        shellAliases = {
+            "fp" = "fd . '/run/media/PRIMARY' | fzf --delimiter='/' --with-nth 5.. --wrap";
+            "fs" = "fd . '/run/media/SECONDARY' | fzf --delimiter='/' --with-nth 5.. --wrap";
+            "fv" = "fd . '/run/media/SECONDARY/nonlinear vault' | fzf --delimiter='/' --with-nth 6.. --wrap";
+            "op" = "open $(fp)";
+            "os" = "open $(fs)";
+            "ov" = "open $(fv)";
+        };
     };
 }
