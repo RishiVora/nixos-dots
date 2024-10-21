44ee993e003a96188959a526e298ba37b18ace17
Rishi <vorarishi22@gmail.com>
2024-10-21 21:01:02 +0530

    starship: update sha

--- home-manager/terminal/utilities.nix
+++ home-manager/terminal/utilities.nix
@@ -27,7 +27,7 @@
                 readFile (
                     fetchurl {
                         url = "https://starship.rs/presets/toml/nerd-font-symbols.toml";
-                        sha256 = "sha256:0k1xk78qa07bl3ml8l88bxbk0wskkrh4ym34my10dj1lrkm16k5a";
+                        sha256 = "sha256:1ka72sz61hk60b2pxrcgxz23cvyaz49sscdj9mzz4ffd3jrcsjzn";
                     }
                 )
             )) // {};
