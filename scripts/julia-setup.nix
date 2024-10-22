f646c51a8c3405a990cdf2e10f502f84bb68c507
Rishi Vora <vorarishi22@gmail.com>
2024-10-23 02:33:06 +0530

    fix(julia): add test for git repo

--- scripts/julia-setup.nix
+++ scripts/julia-setup.nix
@@ -8,6 +8,10 @@
 
             cp $FLAKE/templates/julia.nix ./flake.nix
 
+            if test -d .git
+                ${pkgs.git}/bin/git add flake.nix
+            end
+
             read -P "Add required packages in flake.nix and press Enter to proceed..."
 
             echo "use flake" > .envrc
