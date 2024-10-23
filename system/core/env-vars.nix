024bc2c7079a59e8642972c742bffc08e46ee155
Rishi Vora <vorarishi22@gmail.com>
2024-10-23 19:34:03 +0530

    feat(env): add LD_LIBRARY_PATH

--- system/core/env-vars.nix
+++ system/core/env-vars.nix
@@ -4,5 +4,8 @@
 
         # For nh
         FLAKE = "$HOME/dotfiles/";
+
+        # For OpenGL applications in shell.nix/flake.nix (looking at you GLMakie)
+        LD_LIBRARY_PATH = "/run/opengl-driver/lib:/run/opengl-driver-32/lib";
     };
 }
